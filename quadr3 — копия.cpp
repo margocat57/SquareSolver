0d0f01eacc0b2f0972d991253227361039a6d388
margocat57 <bin242717@gmail.com>
2025-08-25 00:43:42 +0900

    Update quadr3 — копия.cpp
    Последняя версия программы до разбивки на функции

--- quadr3 — копия.cpp
+++ quadr3 — копия.cpp
@@ -1,99 +1,245 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
 
-const char* NO_SOLUTIONS_MSG = "Решений не существует";
+const char* ERROR_INPUT = "Ошибка ввода! Пожалуйста, введите три числа в указанном формате.\n";
+const int INFINITE_SOLUTIONS = 3;
+const double eps = 1e-2;
 
-typedef struct education_params{
+struct equation_params{
     double coef_a;
     double coef_b;
     double coef_c;
-} EDUCATION_PARAMS;
+    const char *msg_error_input;
+    int flg_exit;
+};
 
-typedef struct solutions{
+struct solutions{
     double solution_x1;
     double solution_x2;
-    const char *msg;
-} SOLUTIONS;
+    int number_of_solutions;
+    int linear_eq_flg;
+};
 
-SOLUTIONS solve(const EDUCATION_PARAMS* params) {
-    SOLUTIONS res = {0, 0, NULL};
+solutions solve_linear_equation(const equation_params* params, solutions *res);
 
-    if (params->coef_a == 0) {
-        if (params->coef_b == 0) {
-            if (params->coef_c == 0) {
-                res.msg = "Бесконечное количество решений";
-            } else {
-                res.msg = NO_SOLUTIONS_MSG;
-            }
-        } else {
-            res.solution_x1 = (-params->coef_c) / params->coef_b;
-            res.solution_x2 = res.solution_x1;
+/* рассматриваем случай, когда линейное уравнение вырождается в константу, например
+0*х=3 или 0*х=0 */
+solutions solve_const_equation(const equation_params* params, solutions *res);
+
+double discriminant(const equation_params* params);
+
+solutions solve_quadratic_equation(const equation_params* params);
+
+void clear_input_buffer(void);
+
+int musor(int *start_number, const char* input);
+
+equation_params input_params(void);
+
+void print_linear_solutions(const solutions* print_solution_equation);
+
+void print_quadr_solutions(const solutions* print_solution_equation);
+
+void print_equations(const solutions* sltns);
+
+int comparison(double coef);
+
+int main(void) {
+    setlocale(LC_ALL, "rus");
+
+    puts("Вы используете программу, которая решает квадратные уравнения");
+
+    for(;;) { 
+        equation_params params_main = input_params();
+        if(params_main.flg_exit){
+            break;
         }
+        else if(params_main.msg_error_input != NULL){
+            fprintf(stderr, "\033[31m%s\033[0m\n", ERROR_INPUT);
+            continue;
+        }
+
+        int check=0;
+        printf("Ваше квадратное уравнение %lgx^2 + %lgx + %lg?\n", params_main.coef_a, params_main.coef_b, params_main.coef_c);
+        puts("Введите любое число, если да");
+        if(scanf("%d", &check) != 1){
+            clear_input_buffer();
+            continue;
+        }
+
+        clear_input_buffer();
+
+        solutions solution_equation = solve_quadratic_equation(&params_main);
+        print_equations(&solution_equation);
+    }
+
+    return 0;
+}
+
+
+void clear_input_buffer(void) {
+    int c = 0; 
+    while ((c = getchar()) != '\n' && c != EOF) {;}
+}
+
+
+equation_params input_params(void){
+    equation_params params = {};
+
+    puts("Введите коэффициенты a, b, c уравнения ax^2 + bx + c в любом из удобных форматов: a = 5 b = 6 c = 7 или 5 6 7");
+    puts("Если хотите закончить выполнение программы введите end ");
+    
+    char input[50] = {0};
+    fgets(input, sizeof(input), stdin);
+    int start_number = 0;
+    int ret_value = sscanf(input, "%lf %lf %lf %n", &params.coef_a, &params.coef_b, &params.coef_c, &start_number);
+
+    printf("scanf returned %d" "\n", ret_value);
+    printf("start number: %d" "\n", start_number);
+
+    if(strstr(input, "end")!=NULL){
+        params.flg_exit = 1;
+        return params;
+    }
+    else if(strchr(input, '\n')==NULL){
+        params.msg_error_input = ERROR_INPUT;
+        clear_input_buffer();
+    }
+    else if (sscanf(input, "a = %lf b = %lf c = %lf %n", &params.coef_a, &params.coef_b, &params.coef_c, &start_number) == 3){
+        if (musor(&start_number, input)){
+            params.msg_error_input = ERROR_INPUT;
+            clear_input_buffer();
+        }
+        return params;
+    }
+    else if (sscanf(input, "%lf %lf %lf %n", &params.coef_a, &params.coef_b, &params.coef_c, &start_number) == 3){
+        if (musor(&start_number, input)){
+            params.msg_error_input = ERROR_INPUT;
+            clear_input_buffer();
+        }
+        return params;
     }
     else {
-        double discriminant = params->coef_b * params->coef_b - 4 * params->coef_a * params->coef_c;
+        params.msg_error_input = ERROR_INPUT;
+    }
+    return params;
+}
 
-        if (discriminant < 0) {
-            res.msg = NO_SOLUTIONS_MSG;
+int musor(int *start_number, const char* input){
+    const char* ptr = input;
+    ptr = ptr + *start_number;
+    while(*ptr){
+        if (!isspace(*ptr)){
+            return 1;
         }
-        else if (discriminant == 0) {
-            res.solution_x1 = (-params->coef_b) / (2 * params->coef_a);
-            res.solution_x2 = res.solution_x1;
-        }
-        else {
-            double sqrt_d = sqrt(discriminant);
-            res.solution_x1 = (-params->coef_b + sqrt_d) / (2 * params->coef_a);
-            res.solution_x2 = (-params->coef_b - sqrt_d) / (2 * params->coef_a);
-        }
+    }
+    return 0;
+}
+
+int comparison(double coef){
+    return coef > 0 - eps && coef < 0 + eps;
+}
+
+
+solutions solve_linear_equation(const equation_params* params, solutions *res) {
+
+    if (comparison(params->coef_b)) {
+        return solve_const_equation(params, res);
+    } 
+    res -> number_of_solutions = 1;
+    res -> solution_x1 = (-params->coef_c) / params->coef_b;
+    res -> solution_x2 = res -> solution_x1;
+
+    return *res;
+}
+
+
+solutions solve_const_equation(const equation_params* params, solutions *res){
+
+    if (comparison(params->coef_c)) {
+        res -> number_of_solutions = INFINITE_SOLUTIONS;
+    }
+    else {
+        res -> number_of_solutions = 0;
+    }
+
+    return *res;
+}
+
+
+double discriminant(const equation_params* params){
+    return params->coef_b * params->coef_b - 4 * params->coef_a * params->coef_c;
+}
+
+
+solutions solve_quadratic_equation(const equation_params* params) {
+    solutions res = {0., 0., 0};
+
+    if (comparison(params->coef_a)) {
+        res.linear_eq_flg = 1;
+        return solve_linear_equation(params, &res);
+    }
+    
+    double discriminant_of_this_eq = discriminant(params);
+
+    if (discriminant_of_this_eq < 0 - eps) {
+        res.number_of_solutions = 0;
+    }
+    else if (discriminant_of_this_eq > 0 - eps && discriminant_of_this_eq < 0 + eps) {
+        res.number_of_solutions = 1;
+        res.solution_x1 = (-params->coef_b) / (2 * params->coef_a);
+        res.solution_x2 = res.solution_x1;
+    }
+    else {
+        res.number_of_solutions = 2;
+        double sqrt_d = sqrt(discriminant_of_this_eq);
+        res.solution_x1 = (-params->coef_b + sqrt_d) / (2 * params->coef_a);
+        res.solution_x2 = (-params->coef_b - sqrt_d) / (2 * params->coef_a);
     }
 
     return res;
 }
 
-void clear_input_buffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
+void print_linear_solutions(const solutions* print_solution_equation){
+    switch(print_solution_equation->number_of_solutions){
+        case 0:
+            fprintf(stderr, "\033[31mУравнение не квадратное - решений не существует\033[0m\n");
+            break;
 
-void run_quadratic_equation_solver() {
-    EDUCATION_PARAMS params = {0, 0, 0};
-    
-    printf("Введите коэффициенты a, b, c уравнения ax^2 + bx + c через пробел: ");
-    
-    if (scanf("%lf %lf %lf", &params.coef_a, &params.coef_b, &params.coef_c) != 3) {
-        printf("Ошибка ввода! Пожалуйста, введите три числа.\n");
-        clear_input_buffer(); 
-        return;
-    }
-    
-    clear_input_buffer();
-    
-    SOLUTIONS solution = solve(&params);
-    
-    if (solution.msg != NULL) {
-        printf("%s\n", solution.msg);
-    }
-    else if (solution.solution_x1 == solution.solution_x2) {
-        printf("Решение уравнения: %.2f\n", solution.solution_x1);
-    }
-    else {
-        printf("Решения уравнения: %.2f и %.2f\n", 
-            solution.solution_x1, solution.solution_x2);
+        case 1:
+            fprintf(stderr, "\033[31mУравнение не квадратное - решение уравнения: %.2f\033[0m\n", print_solution_equation->solution_x1);
+            break;
+
+        case INFINITE_SOLUTIONS: 
+                fprintf(stderr, "\033[31mУравнение не квадратное - бесконечное количество решений\033[0m\n"); 
+                break;
     }
 }
 
-int main(void) {
-    setlocale(LC_ALL, "rus");
-    int contin_flg;
+void print_quadr_solutions(const solutions* print_solution_equation){
+    switch(print_solution_equation->number_of_solutions){
+        case 0:
+            puts("Решений не существует\n"); 
+            break;
 
-    do{
-    clear_input_buffer();
-    run_quadratic_equation_solver();
+        case 1:
+            printf("Решение уравнения: %lg\n", print_solution_equation->solution_x1); 
+            break;
 
-    printf("Если хотите решить еще одно уравнение, введите любую цифру\n");
-    }while(scanf("%d", &contin_flg) == 1);
+        case 2: printf("Решения уравнения: %lg и %lg\n", 
+            print_solution_equation->solution_x1, print_solution_equation->solution_x2); 
+            break;
+    }
+}
 
-
-    return 0;
+void print_equations(const solutions* print_solution_equation){
+    if(print_solution_equation->linear_eq_flg){
+        print_linear_solutions(print_solution_equation);
+    }
+    else{
+        print_quadr_solutions(print_solution_equation);
+    }
 }
